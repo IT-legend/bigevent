a27f1b5a1d98ff3ac2900716cdd271d3bc5e61f8
wangmingda <wang521763@163.com>
2020-02-03 22:27:39 +0800

    文章列表编辑

--- web_back/js/util/category.js
+++ web_back/js/util/category.js
@@ -1,4 +1,9 @@
+// 业务逻辑层，只有js代码，没有html代码
+// 不涉及任何页面方面的东西，没有dom操作
+
+
 // 文章列表模块化
+
 // 定义category，将所有与列表有关的方法都写在这个js文件中
 
 
@@ -7,14 +12,23 @@ var category = {
     get:function(){
         return $.get(APILIST.category_get)
     },
-    // 2.添加
+    // 2.添加文章分类
     add:function(name,slug){
         return $.post(APILIST.category_add,{'name':name,'slug':slug})
     },
-    // 删除
+    // 3.删除文章分类
     del:function(id){
         return $.post(APILIST.category_del,{
             'id':id
         })
-    }
+    },
+    // 4.编辑文章分类
+    edit:function(id,name,slug){
+        return $.post(APILIST.category_edit,{
+            'id':id,
+            'name':name,
+            'slug':slug
+        })
+    },
+
 }
--- web_back/js/util/config.js
+++ web_back/js/util/config.js
@@ -2,7 +2,8 @@
 // 把这个项目中所有用到的接口地址全部存在这里
 
 //所有接口的基地址
-var BASEURL = 'http://192.168.5.4:8000'
+// var BASEURL = 'http://192.168.5.4:8000'
+var BASEURL = 'http://192.168.5.4:8000' 
 
 // 用来保存所有的接口信息
 var APILIST = {
@@ -12,12 +13,14 @@ var APILIST = {
     user_getUser: BASEURL + '/admin/getuser',
 
     // 二、文章列表模块
-    // 获取
+    // 获取文章分类
     category_get: BASEURL + '/admin/category_search',
-    // 添加
+    // 添加文章分类
     category_add: BASEURL + '/admin/category_add',
-    // 删除
+    // 删除文章分类
     category_del: BASEURL + '/admin/category_delete',
+    // 编辑文章分类
+    category_edit: BASEURL + '/admin/category_edit',
 
 
     
